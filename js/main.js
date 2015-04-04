b57593b7ec47691d08cb6b941cc6ae980b07a638
trungcl <thanhtrungnait88@gmail.com>
2015-04-05 00:11:30 +0700

    no message

---
+++ js/main.js
@@ -0,0 +1,60 @@
+// Hello.
+//
+// This is The Scripts used for Bloggler Theme
+//
+//
+
+function main() {
+
+(function () {
+   'use strict';
+
+	//Page Loader Script
+	//<![CDATA[
+	//-----------------------------------
+     $(window).load(function() { // makes sure the whole site is loaded
+        $('#status').fadeOut(); // will first fade out the loading animation
+        $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+        $('body').delay(350).css({'overflow':'visible'});
+    });
+    //]]>
+
+    //Main Pages - mCustomScrollbar Plugin Script
+	//-----------------------------------
+    jQuery(document).ready(function(){
+        if(jQuery(window).width() >=768){
+          $("#content").mCustomScrollbar({
+            theme:"inset-2-dark",
+            mouseWheelPixels: 50,
+            scrollInertia: 0
+          });
+        }
+      });
+      jQuery(window).resize(function(){
+        if(jQuery(this).width() >=768){
+          $("#content").mCustomScrollbar({
+            theme:"inset-2-dark",
+            mouseWheelPixels: 50,
+            scrollInertia: 0
+          });
+        }
+        else{
+          $("#content").mCustomScrollbar("destroy");
+        }
+      });
+
+    //Single Blog Post - mCustomScrollbar Plugin Script
+	//-----------------------------------
+	$("#post").mCustomScrollbar({
+          	setHeight: 580,
+          	theme:"inset-2-dark",
+          	mouseWheelPixels: 85,
+          	scrollInertia: 0
+        });
+
+
+}());
+
+
+}
+main();
